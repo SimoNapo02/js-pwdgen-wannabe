3ea5614a312feef79e7005e483dd36979f26a022
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-04-28 10:42:11 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Chiedi all’utente il suo nome,
+const nome= prompt("Inserisci il tuo nome");
+// poi chiedi il suo cognome,
+const cognome= prompt("Inserisci il tuo cognome");
+// poi chiedi il suo colore preferito
+const colore= prompt("Inserisci il tuo colore preferito");
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+document.getElementById("Password").innerHTML = Nome + cognome + colore + 21 
