6b5feef34fd077efa4e7d49f5d3d787ed3ddf1cd
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-04-28 11:45:43 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,4 @@ const cognome= prompt("Inserisci il tuo cognome");
 // poi chiedi il suo colore preferito
 const colore= prompt("Inserisci il tuo colore preferito");
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
-document.getElementById("Password").innerHTML = Nome + cognome + colore + 21 
+document.getElementById("Password").innerHTML = nome + cognome + colore + 21 
